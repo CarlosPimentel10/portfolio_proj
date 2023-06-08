af5847da24b4e8295b7f2bef27431598084e69a5
unknown <carlos.costa.pimentel@gmail.com>
2023-06-08 16:34:38 +0100

    created the superuser

--- portfolioApp/models.py
+++ portfolioApp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.gis.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.gis.geos import Point
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
     home_address = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
-    location = models.PointField()
+    location = models.PointField(default=Point(0, 0))
